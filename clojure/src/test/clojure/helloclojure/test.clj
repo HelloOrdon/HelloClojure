189c3efcd3e8800b851d2d38d868b97e9d71de40
Ordon <ordon1313@gmail.com>
2015-04-16 10:46:56 +0200

    example

--- clojure/src/test/clojure/helloclojure/test.clj
+++ clojure/src/test/clojure/helloclojure/test.clj
@@ -2,3 +2,8 @@
   (:use midje.sweet, helloclojure.core))
 
 (fact (testfn "world") => "Hello world")
+(fact -1 => ((if (> 2 3) + -) 5 6))
+(fact [4,5] => (filter (fn [x] (> x 3)) [1,2,3,4,5]))
+(fact [[1,1]] => (filter (fn [[x,y]] (= x y)) [[1,1],[2,3],[4,5]]))
+
+
