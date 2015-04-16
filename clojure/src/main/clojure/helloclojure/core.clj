fda0754bccc89dcb9b1820cc2dd9af095903b88a
Ordon <ordon1313@gmail.com>
2015-04-16 18:04:03 +0200

    name

--- clojure/src/main/clojure/helloclojure/core.clj
+++ clojure/src/main/clojure/helloclojure/core.clj
@@ -1,4 +1,13 @@
 (ns helloclojure.core)
 
+(defn in?
+  "true if seq contains elm"
+  [seq elm]
+  (some #(= elm %) seq))
+
 (defn testfn [a]
   (str "Hello " a))
+
+(defn tick [x]
+  (if (> (count x) 1) x [] )
+  )
